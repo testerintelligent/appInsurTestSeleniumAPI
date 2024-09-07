d552fc3d8e2482f5ce4b85fc125145f9d9d5c31f
Thamarai <atchandiran@gmail.com>
2024-09-08 02:47:05 +0530

    runnerfile

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,12 +1,16 @@
 package runner;
 
-
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "stepDefenition",
-        plugin = {"pretty", "html:target/cucumber-reports.html"}
+        plugin = {"pretty", "html:target/cucumber-reports-html.html",
+                "json:target/report.json"
+        }
 )
 
 public class TestRunner {
