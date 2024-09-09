d87607bc81adad6f398f52ee8e76afbed352755e
Thamarai <atchandiran@gmail.com>
2024-09-10 01:36:02 +0530

    config changes

--- src/main/java/Config/ReactAppConfig.java
+++ src/main/java/Config/ReactAppConfig.java
@@ -1,10 +1,16 @@
 package Config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public abstract class ReactAppConfig {
     public WebDriver getDriver() {
+
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remote-allow-origins=*");
+//        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
