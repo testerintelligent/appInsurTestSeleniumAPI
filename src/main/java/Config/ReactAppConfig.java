9459268f97adaebd3d9a20ad25b94c255e40382c
Thamarai <atchandiran@gmail.com>
2024-09-10 01:52:03 +0530

    config changes

--- src/main/java/Config/ReactAppConfig.java
+++ src/main/java/Config/ReactAppConfig.java
@@ -1,17 +1,21 @@
 package Config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public abstract class ReactAppConfig {
     public WebDriver getDriver() {
-
-//        ChromeOptions chromeOptions = new ChromeOptions();
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tg2631\\IdeaProjects\\ReactApp_RestAPI\\src\\main\\resources\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.addArguments("--remote-allow-origins=*");
 //        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+            WebDriver driver = new ChromeDriver(chromeOptions);
+            String userAgent = (String) ((JavascriptExecutor) driver).executeScript("return navigator.userAgent;");
+            System.out.println("Chrome version " + userAgent);
 
         driver.manage().window().maximize();
         driver.get("http://192.168.99.141:3000/");
