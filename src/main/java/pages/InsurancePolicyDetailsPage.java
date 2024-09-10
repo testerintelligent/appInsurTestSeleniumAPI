8b4d8ed59c26dca8d7b49e574e7434a32477c6c9
Thamarai <atchandiran@gmail.com>
2024-09-10 17:56:21 +0530

    remove unused imports

--- src/main/java/pages/InsurancePolicyDetailsPage.java
+++ src/main/java/pages/InsurancePolicyDetailsPage.java
@@ -3,10 +3,6 @@ package pages;
 import Config.DriverComponents;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class InsurancePolicyDetailsPage extends DriverComponents {
     WebDriver driver;
